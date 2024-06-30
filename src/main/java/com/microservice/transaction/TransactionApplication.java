8d42f90e05de361b099758e9a9410b905760d224
Himanshu Singh Dhapola <himanshu.dhapola@xebia.com>
2024-06-30 12:19:14 +0530

    Health Check Controller added

--- src/main/java/com/microservice/transaction/TransactionApplication.java
+++ src/main/java/com/microservice/transaction/TransactionApplication.java
@@ -1,13 +1,20 @@
 package com.microservice.transaction;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TransactionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionApplication.class, args);
+	}
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
