ccf97d6fc87b78df4063fc35a7f5e98afe2f1206
Ayaan Lone <ayaan.lone@xebia.com>
2024-07-24 19:35:01 +0530

    Transaction Via Card Added

---
+++ src/main/java/com/onlineBanking/transaction/client/CardClientHandler.java
@@ -0,0 +1,56 @@
+package com.onlineBanking.transaction.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.onlineBanking.transaction.entity.CardType;
+
+@Component
+public class CardClientHandler {
+	@Value("${card.service.url}")
+	private String cardServiceUrl;
+
+	private final RestTemplate restTemplate;
+
+	public CardClientHandler(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public CardType fetchCardType(long userId, long cardNumber) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		String url = cardServiceUrl + "fetchCardType?userId=" + userId + "&cardNumber=" + cardNumber;
+
+		ResponseEntity<CardType> response = restTemplate.exchange(url, HttpMethod.GET, null, CardType.class);
+		return response.getBody();
+	}
+
+	public double fetchCardBalance(long userId, long cardNumber) {
+		String url = cardServiceUrl + "fetchCardBalance?userId=" + userId + "&cardNumber=" + cardNumber;
+
+		 ResponseEntity<Double> response = restTemplate.exchange(url, HttpMethod.GET, null, Double.class);
+
+		return response.getBody();
+	}
+	
+	public String updateCardBalance(long userId, long cardNumber, double amount) {
+		
+		String url = cardServiceUrl + "update-balance?userId=" + userId + "&cardNumber=" + cardNumber + "&amount="+amount;
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+
+		ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST,null, String.class);
+
+		return response.getBody();
+	}
+
+
+}
--- src/main/java/com/onlineBanking/transaction/util/ConstantUtils.java
+++ src/main/java/com/onlineBanking/transaction/util/ConstantUtils.java
@@ -8,6 +8,9 @@ public class ConstantUtils {
 	public static final String USER_NOT_EXIST = "No User with this id Exists";
 	public static final String INVALID_MONTH = "Month name should be before current month or the current month itself";
 	public static final String TRANSACTIONS_NOT_FOUND = "No Transactions Found";
+	public static final String TRANSACTION_FAILED = "TRANSACTION FAILED";
+	public static final String INSUFFICIENT_BALANCE = "Insufficient Balance";
+	public static final String INVALID_CARD_TYPE = "Invalid Card Type";
 	
 
 }
